f728e6eba9327085851e5447fea6468b122aa4ee
yan160100 <1909484942@qq.com>
2023-03-20 15:43:59 +0800

    添加内容

--- java-publisher/src/main/java/com/yan/publisher/JavaPublisherApplication.java
+++ java-publisher/src/main/java/com/yan/publisher/JavaPublisherApplication.java
@@ -1,24 +1,12 @@
 package com.yan.publisher;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class JavaPublisherApplication {
 
-    public static void main(String[] args) throws IOException, TimeoutException {
-
-        RabbitTemplate rabbitTemplate = new RabbitTemplate();
-        // 队列名称
-        String queueName = "simple.queue";
-        // 消息
-        String message = "hello, spring amqp!";
-        // 发送消息
-        rabbitTemplate.convertAndSend(queueName, message);
+    public static void main(String[] args) {
         SpringApplication.run(JavaPublisherApplication.class, args);
     }
 
---
+++ java-publisher/src/main/java/com/yan/publisher/controller/SendController.java
@@ -0,0 +1,26 @@
+package com.yan.publisher.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yan
+ **/
+@RestController
+public class SendController {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/send")
+    public void list() {
+        String queue = "simple.queue";
+        // 消息
+        String msg = "hello, spring amqp!";
+        // 发送消息
+        rabbitTemplate.convertAndSend(queue, msg);
+    }
+
+}
