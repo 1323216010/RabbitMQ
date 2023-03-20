a0cf943502eff4b2820f58acf56f8d9a6abe2095
yan160100 <1909484942@qq.com>
2023-03-20 15:34:21 +0800

    添加.gitignore文件

--- java-consumer/src/main/java/com/yan/consumer/Listener/Listener.java
+++ java-consumer/src/main/java/com/yan/consumer/Listener/Listener.java
@@ -1,11 +1,12 @@
 package com.yan.consumer.Listener;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RabbitListener {
+public class Listener {
 
-    @org.springframework.amqp.rabbit.annotation.RabbitListener(queues = "simple.queue")
+    @RabbitListener(queues = "simple.queue")
     public void listenSimpleQueueMessage(String msg) throws InterruptedException {
         System.out.println("消费者接收到消息：【" + msg + "】");
     }
